e8dab79dfc72e3cc6b0abd9142576054c32e6d51
Krissy-Flora <karissa.flora@gmail.com>
2016-06-15 12:10:09 -0700

    end of task 17, loads data when the page loads

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,9 +1,4 @@
 import Ember from 'ember';
-var Photo = Ember.Object.extend({
-	title: '',
-	username: '',
-	url: '',
-});
 
 var PhotoCollection = Ember.ArrayProxy.extend(Ember.SortableMixin, {
 	sortProperties: ['title'],
@@ -15,26 +10,12 @@ export default Ember.Controller.extend({
 	photos: PhotoCollection.create(),
 	searchField: '',
 	tagsearchField: '',
+	tagList: ['hi','cheese'],
 	filteredPhotos: function(){
 		var filter = this.get('searchFilter');
 		var rx = new RegExp(filter, 'gi');
-		var Photo = Ember.Object.extend({
-				title: '',
-				username: '',
-				//flickr extra data
-				owner: '',
-				//flickr url data
-				id: '',
-				farm: 0,
-				secret: '',
-				server: '',
-				url: function(){
-					return "https://farm"+this.get('farm')+
-					".staticflickr.com/"+this.get('server')+
-					"/"+this.get('id')+"_"+this.get('secret')+"_b.jpg";
-				}.property('farm','server','id','secret'),
-		});
 		var photos = this.get('photos');
+		
 		return photos.filter(function(photo){
 			return photo.get('title').match(rx) || photo.get('username').match(rx);
 		});
@@ -71,5 +52,26 @@ export default Ember.Controller.extend({
 				});
 			});
 		},
+		clicktag: function(tag){
+			this.set('tagSearchField', tag);
+			this.get('photos').content.clear();
+			this.store.unloadAll('photo');
+			this.send('getPhotos',tag);
+		}
+	},
+	init:function(){
+			this._super.apply(this, arguments);
+			var apiKey = '4435e3a217bc7afc94dfcba607b70eb1';
+		var host = 'https://api.flickr.com/services/rest/';
+		var method = "flickr.tags.getHotList";
+		var requestURL = host + "?method="+method + "&api_key="+apiKey+"&count=75&format=json&nojsoncallback=1";
+		var t = this;
+		Ember.$.getJSON(requestURL, function(data){
+			//callback for successfully completed requests
+			console.log(data);
+			data.hottags.tag.map(function(tag) {
+				t.get('tagList').pushObject(tag._content);
+			});
+		});
 	}
 });
